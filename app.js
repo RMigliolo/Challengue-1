c2f8ae0b93d12c6ea7b28ba80a4986e790a4c1c9
RMigliolo <167660688+RMigliolo@users.noreply.github.com>
2025-03-17 12:49:44 -0600

    1.1 Añade función agregar nombre amigo

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+//Array almacenar nombres amigos
 let amigos = [] 
 
+//Función agregar nombre amigo
+function agregarAmigo() {
+    let inputAmigo = document.getElementById('amigo');
+    let nombreAmigo = inputAmigo.value;
+    
+    if (inputAmigo == nombreAmigo) {
+     alert('Ingrese un nombre');
+     return;
+    }
 
-function agregarAmigo()        
+    amigos.push(nombreAmigo);
+    inputAmigo.value = '';
+    inputAmigo.focus();
+    console.log(amigos);
+}
+
+
+
+
+
