68c19cb9c5469afedaddb19d49c88f26b025e685
RMigliolo <167660688+RMigliolo@users.noreply.github.com>
2025-03-17 14:51:06 -0600

    Añade función para sortear un amigo y manejar el caso de lista vacía

--- app.js
+++ app.js
@@ -30,5 +30,16 @@ function listadoAmigos() {
     }
 }
 
+function sortearAmigo(){
+    if(amigos.length === 0){
+        alert('No hay amigos para sortear');
+        return;
+    }
+    let amigoSorteado = amigos[Math.floor(Math.random*amigos.length)];
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
+
+
+}
 
 
