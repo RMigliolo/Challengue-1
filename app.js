4f510bf459a3a7da8253895e1d2f8e5f8a118d6a
RMigliolo <167660688+RMigliolo@users.noreply.github.com>
2025-03-17 15:20:19 -0600

    Añade validación para evitar nombres duplicados y mejora la limpieza de la lista de amigos

--- app.js
+++ app.js
@@ -7,19 +7,24 @@ let amigos = []
 function agregarAmigo() {
     let inputAmigo = document.getElementById('amigo');
     let nombreAmigo = inputAmigo.value;
-    
+    //Validación de campo vacío en input
     if (inputAmigo.value === '') {
      alert('Ingrese un nombre');
-     return;
+     return; //Detiene ejecución de la función
     }
 
+    if(amigos.includes(nombreAmigo) {
+        alert('El nombre del amigo ya fue ingresado');
+        return;
+    }
+    //Seguimiento de los nombres ingresados, limpieza de input y focus (cursor)
     amigos.push(nombreAmigo);
     inputAmigo.value = '';
     inputAmigo.focus();
     listadoAmigos();
     console.log(amigos);
 }
-
+//Función crear el listado de amigos
 function listadoAmigos() {
     let listaAmigos = document.getElementById('listaAmigos');
     listaAmigos.innerHTML = '';
@@ -29,17 +34,16 @@ function listadoAmigos() {
         listaAmigos.appendChild(item);
     }
 }
-
+//Función sorteo de amigo
 function sortearAmigo(){
     if(amigos.length === 0){
         alert('No hay amigos para sortear');
         return;
     }
-    let amigoSorteado = amigos[Math.floor(Math.random*amigos.length)];
+    let amigoSorteado = amigos[Math.floor(Math.random()*amigos.length)];
     let resultado = document.getElementById('resultado');
     resultado.innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
-
-
+//Función limpiar lista
+    let limpiarLista = document.getElementById('listaAmigos');
+    limpiarLista.innerHTML = '';
 }
-
-
